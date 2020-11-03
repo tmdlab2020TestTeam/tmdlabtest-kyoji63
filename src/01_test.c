4b4b55f2ba0033f46ae61c205e9c73297b780c4e
higuchi kaito <kyoji0603@kyoji0603.local>
2020-11-03 14:13:33 +0900

    update 01test

--- src/01_test.c
+++ src/01_test.c
@@ -18,7 +18,10 @@ void bubble_sort(int num[total_num]){
   for(array_num=0; array_num<total_num; array_num++){
     for(i=total_num-1; i>array_num; i--){
       if(num[i] < num[i-1]){
-        /* 配列前後の値を入れ替える．tmpで一時的に値を退避させておく．*/
+        // 配列前後の値を入れ替える．tmpで一時的に値を退避させておく．
+         tmp = num[i];
+         num[i] = num[i-1];
+         num[i-1] = tmp;
       }
     }
   }
@@ -33,7 +36,13 @@ void prime_number(int num[total_num]){
   int prime_num[total_num];    // 該当する素数をここに格納していく
   for(array_num=0; array_num<total_num; array_num++){
     flag = 0;
-    /* for文で2倍3倍..とnum[array_num]まで回して，num[array_num]が割り切れた場合flagを立てる．*/
+    // for文で2倍3倍..と回して，nの倍数だった場合flagを立てる．
+     for(int i=2; i<num[array_num]; i++){
+       if(num[array_num]%i == 0){
+         flag = 1;
+         break;
+       }
+     }
     if(flag==0){
       prime_num[count] = num[array_num];
       count++;
